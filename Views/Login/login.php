b81678959cd1f00a0b280ca435ff5bda7bbae5d5
gerardo2904 <gerardo2904@hotmail.com>
2023-09-05 19:43:54 -0700

    login

--- Views/Login/login.php
+++ Views/Login/login.php
@@ -18,7 +18,7 @@
     </section>
     <section class="login-content">
       <div class="logo">
-        <!-- <h1><?= $data['page_title']; ?></h1> -->
+        <!-- <h1><?= $data['page_title']; ?></h1> --> 
         <img src="<?= media(); ?>/images/eye-chart-eye-examination.png" class="loguito">  
       </div>
       <div class="login-box">
